9b747d286d57879b3f3999cd9bb4416e9efaa976
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-03-16 21:22:40 -0300

    Chain group added

--- src/components/EachRestaurant/EachRestaurant.js
+++ src/components/EachRestaurant/EachRestaurant.js
@@ -7,29 +7,47 @@ import axios from 'axios';
 function EachRestaurant(props) {
 
   const [restaurantDetails, setRestaurantDetails] = useState({});
+  const [chainName, setChainName] = useState({});
 
   const getSpecificRestaurant = async () => {
     try{
       const { params } = props.match;
       let response = await axios.get(`http://localhost:5000/api/restaurants/${params.restaurantId}`);
       setRestaurantDetails(response.data.restaurant);
-      console.log('response', response)
-    }catch{
-      console.log('Error while accessing a specific restaurant')
+      getChainName(response.data.restaurant.chain)
+    }catch(err){
+      console.log('Error while accessing a specific restaurant:', err)
     }
 
+  }
+
+  const getChainName = async (id) => {
+    try{
+      let response = await axios.get(`http://localhost:5000/api/chains/${id}`)
+      setChainName(response.data.chain)
+    } catch(err){
+      console.log('Error while getting chain name', err)
+    }
   }
 
   useEffect(() => {
     getSpecificRestaurant()
   }, [])
 
+  console.log('chain', chainName)
 
   return (
     <div className="home">
-    <h2>{restaurantDetails.name}</h2>
-    <h3>{restaurantDetails.location}</h3>
-    <h4>{restaurantDetails.chain}</h4>
+    <div>
+    <h2>Name: {restaurantDetails.name}</h2>
+    <h3>Location: {restaurantDetails.location}</h3>
+    <h4>Chain: {chainName.name}</h4>
+    </div>
+    <div>
+  <Link to={'/api/restaurants'}>
+      <Button color='teal'>Return</Button>
+    </Link>
+  </div>
     </div>
   );
 }
